13c59b5b917020c251d803a4f524d53c577054c7
Kristina <kr.hranovska@gmail.com>
2024-02-07 16:17:02 +0200

    update

--- src/favorites.js
+++ src/favorites.js
@@ -1,3 +1,3 @@
-import './js/Menu/burger-menu'
-import './js/Quote/quote'
-import './js/Favorities/favorities'
+import './js/menu/burger-menu'
+import './js/quote/quote'
+import './js/favorities/favorities'
