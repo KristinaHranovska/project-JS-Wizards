961c268b068804f131dd6a663fb694af1b6ef95e
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2024-02-09 22:47:55 +0200

    add style card, markup file.html changes file.js

--- src/js/favorities.js
+++ src/js/favorities.js
@@ -1,8 +1,11 @@
 // const favoritesContainer = document.getElementById('favorites-container');
+//const removeCards = document.querySelector(".container-remove-favorites");
+// const infoFavorite = document.querySelector(".list-info-favorites");
+// const deleteFavorite = document.querySelector(".delete-favorites");
 
 // function savedCardsStorage() {
 //     try {
-//         const savedCards = JSON.parse(localStorage.getItem());
+//         const savedCards = JSON.parse(localStorage.getItem()) || [];
 //         displayFavoriteCards(savedCards);
 //     } catch(error) {
 //         errorCardStorage(error);
@@ -13,13 +16,12 @@
 //     favoritesContainer.innerHTML = "";
 
 //     if (!savedCards || savedCards.length === 0) {
-//         return favoritesContainer.innerHTML = `
-//         <img src="./favorites_1440.webp" alt="">
-//         <p>It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.</p>
-//         `
+        // removeCards.classList.remove("is-hidden");
 //     } else {
+// removeCards.classList.add("is-hidden");
+ //     createTaskMarkup(obj)
 //         const markup = renderExerciseCard(savedCards);
-//         favoritesContainer.insertAdjacentHTML("beforeend", markup);
+//     
 
 //          const deleteButtons = document.querySelectorAll('.delete-button');
 //          deleteButtons.forEach(button => {
@@ -34,22 +36,27 @@
 // function renderExerciseCard(exercise) {
 //    return exercise.map(({ name, burnedCalories, bodyPart, target }) => {
 //        return `
-//     <div class="card-list>
-//     <h3>${name}<h3>
-//     <p>Burned calories: ${burnedCalories} / 3 min</p>
-//     <p>Body part: ${bodyPart}<p>
-//     <p>Target: ${target}</p>
+//     <li> 
+//     <div class="container-cards-favorites">
+//       <svg class="icon-parts-fitness" width="24" height="24">
+//         <use href="./img/icons/sprite.svg#icon-body-parts-fitness"></use>
+//       </svg>
+//       <h3 class="subtitle-favorites">${name}</h3>
 //     </div>
-//         `;
-//     }).join("");
-
+//     <div class="subtext-container">
+//       <p class="subtext-favorites">${burnedCalories} / 3 min</p>
+//       <p class="subtext-favorites">${bodyPart}: Waist</p>
+//     </div>
+//     <p class="subtext-favorites">${target}: Quads</p>
+//   </li >`;
+//}).join("");
 // }
 
 // function removeFavoriteCard(index) {
 //     let savedCards = JSON.parse(localStorage.getItem('savedCards')) || [];
 //     savedCards.splice(index, 1);
 //     localStorage.setItem('savedCards', JSON.stringify(savedCards));
-//     savedCardsStorage(); 
+//     savedCardsStorage();
 // }
 
 
@@ -60,4 +67,38 @@
 //         title: 'Error',
 //         message: 'Wrong operation!!!',
 //     });
+// }
+
+
+// function createTaskMarkup(obj) {
+//    return `
+//       <li class="list-favorites-item">
+//           <div class="container-card">
+//             <div class="container-worcaut">
+//               <button class="workout">Workout</button>
+//               <svg class="delete-favorites" data-id="${id}" width="16" height="16">
+//                 <use href="./img/icons/sprite.svg#icon-favorites-delete"></use>
+//               </svg>
+//               <button type="button" class="button-strart">
+//                 Start
+//                 <svg class="icon-arrow-body" width="14" height="14">
+//                   <use
+//                     href="./img/icons/sprite.svg#icon-arrow-body-parts"
+//                   ></use>
+//                 </svg>
+//               </button>
+//             </div>
+//             <ul class="list-info-favorites">
+//             </ul>
+//           </div>
+//         </li>
+//     ` 
+//      favoritesContainer.insertAdjacentHTML("beforeend", markup);
+// }
+
+// deleteFavorite.addEventListener("click", deleteTask);
+// function deleteTask(evt) {
+//     if (evt.target.nodeName === "BUTTON") {
+//         const id = evt.target.dataset.id;
+// }
 // }
