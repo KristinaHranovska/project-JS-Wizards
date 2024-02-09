17815a1daeae84ab6d105754a191d7a0ab780fa3
OlhaKatrych <olgak198931@gmail.com>
2024-02-09 23:49:19 +0200

    muscles

--- src/js/helper/helpers.js
+++ src/js/helper/helpers.js
@@ -1,24 +1,43 @@
 const iziToastFunctions = {
-    getInfo(messageInfo) {
-        iziToast.info({
-            title: 'Hello',
-            message: messageInfo,
-        });
-    },
+  getInfo(messageInfo) {
+    iziToast.info({
+      title: 'Hello',
+      message: messageInfo,
+    });
+  },
 
-    getErrorInfo(messageInfo) {
-        iziToast.error({
-            title: 'Error',
-            message: messageInfo,
-        });
-    },
+  getErrorInfo(messageInfo) {
+    iziToast.error({
+      title: 'Error',
+      message: messageInfo,
+    });
+  },
 
-    getSuccessInfo(messageInfo) {
-        iziToast.success({
-            title: 'OK',
-            message: messageInfo,
-        });
-    }
+  getSuccessInfo(messageInfo) {
+    iziToast.success({
+      title: 'OK',
+      message: messageInfo,
+    });
+  },
 };
 
-export { iziToastFunctions }
+function createMarkupFilterMuscles(muscles, galleryContainer) {
+  const markup = muscles
+    .map(
+      ({ filter, imgUrl, name }) => `
+      <li class="lists js-list">
+      <img src="${imgUrl}" alt="${name}"class="pic">
+      <div class="position-text">
+      <h2 class="muscles-title">${name}</h2>
+      <p class="filter-title">${filter}</p>
+      </div>
+      </li>`
+    )
+    .join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+export { createMarkupFilterMuscles };
+
+export { iziToastFunctions };
