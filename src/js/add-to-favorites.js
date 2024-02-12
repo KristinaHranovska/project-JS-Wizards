e8a528a3f9a826ba81b55f904a3b945bf4927288
VladislavFedorchuk <142826785+VladislavFedorchuk@users.noreply.github.com>
2024-02-13 00:53:40 +0200

    finally

--- src/js/add-to-favorites.js
+++ src/js/add-to-favorites.js
@@ -1,54 +1,52 @@
-const modalExercises = document.querySelector('.button');
-if (modalExercises) {
-  const addToFavoritesBtn = modalExercises.querySelector('.modal-button-favorites');
-  addToFavoritesBtn.addEventListener('click', addToFavorites);
-} else {
-}
-
-
-export const LOCAL_STORAGE_KEY = 'favoriteData';
-export let inLocalStorage;
-
-async function addToFavorites(e) {
-  try {
-    if (e.target.classList.contains('modal-button-favorites')) {
-      inLocalStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
-      const itemId = await getData(e.target.dataset.id);
-      const data = itemId.data;
-      const findCopy = inLocalStorage.some(item => item._id === data._id);
-
-      if (!findCopy) {
-
-        const itemName = e.target.getAttribute('data-name');
-        const newData = { _id: itemId, name: itemName };
-        inLocalStorage.push(newData);
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(inLocalStorage)); 
-      } else {
-        return;
-      }
-    
-      setTimeout(() => {
-        modalExercises.classList.remove('is-open');
-      }, 550);
-
-      body.classList.remove('body-modal');
+const addToFavoritesBtn = document.querySelector('.add-to-favorities');
+const removeFromFavoritesBtn = document.querySelector('.js-remove');
+const modal = document.querySelector('.modal-window');
+const idModul = document.querySelector('.js-gallery');
+const storedArrayAdd = JSON.parse(localStorage.getItem('addKeyID')) || [];
+const storedArrayRemove = JSON.parse(localStorage.getItem('addKremoveKeyIDeyID')) || [];
+let cardID;
+idModul.addEventListener('click', getIDModul);
+function getIDModul(e) {
+    const liElement = e.target.closest('.js-id');
+    if (liElement) {
+        cardID = liElement.dataset.id;
+        if (storedArrayAdd.includes(cardID)) {
+            removeFromFavoritesBtn.classList.remove('hidden-btn');
+            addToFavoritesBtn.classList.add('hidden-btn');
+        } else {
+            removeFromFavoritesBtn.classList.add('hidden-btn');
+            addToFavoritesBtn.classList.remove('hidden-btn');
+        }
     }
-  } catch (error) {
-    console.log(error.message);
-  } finally {
-    
-  }
 }
-function addToLocalStorage(id) {
-  try {
-    const localStorageData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
-    const isIdInStorage = localStorageData.some(item => item._id === id);
-    if (!isIdInStorage) {
-      const newData = { _id: id };
-      localStorageData.push(newData);
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(localStorageData));
+if (!localStorage.getItem('addKeyID')) {
+    const emptyArrayAdd = [];
+    localStorage.setItem('addKeyID', JSON.stringify(emptyArrayAdd));
+}
+if (!localStorage.getItem('removeKeyID')) {
+    const emptyArrayRemove = [];
+    localStorage.setItem('removeKeyID', JSON.stringify(emptyArrayRemove));
+}
+addToFavoritesBtn.addEventListener('click', getIdFavorites);
+function getIdFavorites() {
+    removeFromFavoritesBtn.classList.remove('hidden-btn');
+    addToFavoritesBtn.classList.add('hidden-btn');
+    const storedArrayFavorites = JSON.parse(localStorage.getItem('addKeyID'));
+    storedArrayFavorites.push(cardID);
+    localStorage.setItem('addKeyID', JSON.stringify(storedArrayFavorites));
+}
+removeFromFavoritesBtn.addEventListener('click', removeIdFavorites)
+function removeIdFavorites() {
+    console.log('click')
+    console.log(cardID)
+    removeFromFavoritesBtn.classList.add('hidden-btn');
+    addToFavoritesBtn.classList.remove('hidden-btn');
+    const storedArrayRemove = JSON.parse(localStorage.getItem('removeKeyID'));
+    storedArrayRemove.push(cardID);
+    localStorage.setItem('removeKeyID', JSON.stringify(storedArrayRemove));
+    const index = storedArrayAdd.indexOf(cardID);
+    if (index !== -1) {
+        storedArrayAdd.splice(index, 1);
+        localStorage.setItem('addKeyID', JSON.stringify(storedArrayAdd));
     }
-  } catch (error) {
-    console.error('Помилка під час додавання ідентифікатора до локального сховища:', error);
-  }
 }
