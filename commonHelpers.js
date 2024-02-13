8d3cd4b80c3853f70675eaf0f547935e2ba77bc5
KristinaHranovska <127331857+KristinaHranovska@users.noreply.github.com>
2024-02-13 19:56:58 +0000

    Deploying to gh-pages from @ KristinaHranovska/project-JS-Wizards@51d5c23c3d25991f20738c02b780edd4b8989555 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,35 +1,35 @@
-import{i as d,g as l,a as r}from"./assets/modal-window-c590c70a.js";import"./assets/vendor-0a7c832e.js";const o={favoritesCard:document.getElementById("favorites-container"),removeCards:document.querySelector(".container-remove-favorites"),deleteButtons:document.querySelectorAll(".button-remove"),galleryWindow:document.querySelector(".js-gallery")};function v(){try{const e=JSON.parse(localStorage.getItem("addKeyID"))||[];console.log(e),u(e)}catch{d.getErrorInfo("Wrong operation!!!")}}function u(e){!e||e.length===0?n():(p(e),g(),h())}o.galleryWindow.addEventListener("click",f);function f(e){if(e.target.classList.contains("js-remove-favorites")){const t=e.target.closest(".list-favorites-item").dataset.id;m(t),e.target.closest(".list-favorites-item").remove(),(JSON.parse(localStorage.getItem("addKeyID"))||[]).length===0&&n()}}function m(e){let t=JSON.parse(localStorage.getItem("addKeyID"))||[];t=t.filter(s=>s!==e),localStorage.setItem("addKeyID",JSON.stringify(t))}function n(){o.removeCards.classList.remove("is-hidden")}function g(){o.removeCards.classList.add("is-hidden")}function h(){const e=document.querySelector(".scroll");if(e){const t=e.getBoundingClientRect().height;window.scrollBy({top:t*1,behavior:"smooth"})}}v();function p(e){Promise.all(e.map(t=>l({typeFilter:"exercises",id:t}))).then(t=>{const s=t.map(a=>a.data);o.favoritesCard.insertAdjacentHTML("beforeend",y(s))}).catch(t=>console.error(t))}function y(e){return e.map(({name:t,target:s,bodyPart:a,burnedCalories:c,_id:i})=>`<li class="list-favorites-item js-id" data-id="${i}">
+import{g as l,i as a}from"./assets/modal-window-029d5099.js";import{i}from"./assets/vendor-0a7c832e.js";const v={getInfo(e){i.info({title:"Hello",message:e})},getErrorInfo(e){i.error({title:"Error",message:e})},getSuccessInfo(e){i.success({title:"OK",message:e})}},o={favoritesCard:document.getElementById("favorites-container"),removeCards:document.querySelector(".container-remove-favorites"),deleteButtons:document.querySelectorAll(".button-remove"),galleryWindow:document.querySelector(".js-gallery")};function f(){try{const e=JSON.parse(localStorage.getItem("addKeyID"))||[];console.log(e),u(e)}catch{v.getErrorInfo("Wrong operation!!!")}}function u(e){!e||e.length===0?c():(y(e),h(),p())}o.galleryWindow.addEventListener("click",m);function m(e){if(e.target.classList.contains("js-remove-favorites")){const t=e.target.closest(".list-favorites-item").dataset.id;g(t),e.target.closest(".list-favorites-item").remove(),(JSON.parse(localStorage.getItem("addKeyID"))||[]).length===0&&c()}}function g(e){let t=JSON.parse(localStorage.getItem("addKeyID"))||[];t=t.filter(s=>s!==e),localStorage.setItem("addKeyID",JSON.stringify(t))}function c(){o.removeCards.classList.remove("is-hidden")}function h(){o.removeCards.classList.add("is-hidden")}function p(){const e=document.querySelector(".scroll");if(e){const t=e.getBoundingClientRect().height;window.scrollBy({top:t*1,behavior:"smooth"})}}f();function y(e){Promise.all(e.map(t=>l({typeFilter:"exercises",id:t}))).then(t=>{const s=t.map(r=>r.data);o.favoritesCard.insertAdjacentHTML("beforeend",b(s))}).catch(t=>console.error(t))}function b(e){return e.map(({name:t,target:s,bodyPart:r,burnedCalories:d,_id:n})=>`<li class="list-favorites-item js-id" data-id="${n}">
     <div class="container-card">
         <div class="container-worcaut">
             <div class="workout-thumb">
             <div class="workout">Workout</div>
             <button type="button" class="button-remove js-remove-favorites">
-            <svg class="delete-favorites" data-id="${i}" width="16" height="16">
-                <use href="${r}#icon-favorites-delete"></use>
+            <svg class="delete-favorites" data-id="${n}" width="16" height="16">
+                <use href="${a}#icon-favorites-delete"></use>
             </svg>
             </button>
             </div>
             <button type="button" class="button-start js-start">
                 Start
                 <svg class="icon-arrow-body js-start" width="14" height="14">
-                    <use href="${r}#icon-arrow-body-parts"></use>
+                    <use href="${a}#icon-arrow-body-parts"></use>
                 </svg>
             </button>
         </div>                  
                 <div class="container-cards-favorites">
                     <svg class="icon-parts-fitness" width="24" height="24">
-                        <use href="${r}#icon-body-parts-fitness"></use>
+                        <use href="${a}#icon-body-parts-fitness"></use>
                     </svg>
                     <h3 class="subtitle-favorites">${t.charAt(0).toUpperCase()+t.slice(1)}</h3>
                 </div>
                 <div class="container-subtext-info">                           
                    <div class="favorites-card-text-wrapper"> 
                    <h4 class="subtext-info">Burned calories:</h4>  
-                    <p class="mini-info">${c}/ 3 min</p>  
+                    <p class="mini-info">${d}/ 3 min</p>  
                     </div>
                     <div class="favorites-card-text-wrapper">
                     <h4 class="subtext-info">Body part:</h4> 
-                    <p class="mini-info">${a}</p>  
+                    <p class="mini-info">${r}</p>  
                     </div>
                     <div class="favorites-card-text-wrapper">
                     <h4 class="subtext-info">Target: </h4>
