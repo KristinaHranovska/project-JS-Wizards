ea9434aadbf319903d632dfad54f7b8dad5f98c7
AntonHran <127331857+KristinaHranovska@users.noreply.github.com>
2024-02-13 22:17:34 +0200

    add button

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -2,31 +2,31 @@ const favoritesButton = document.querySelector('.link-favorites');
 const homeButton = document.querySelector('.link-home');
 
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector(".burger-menu-open"),
-        closeModalBtn: document.querySelector(".burger-closed"),
-        modal: document.querySelector(".backdrop"),
-    };
+  const refs = {
+    openModalBtn: document.querySelector('.burger-menu-open'),
+    closeModalBtn: document.querySelector('.burger-closed'),
+    modal: document.querySelector('.backdrop'),
+  };
 
-    const backdrop = document.querySelector(".backdrop");
-    backdrop.classList.add("is-hidden");
+  const backdrop = document.querySelector('.backdrop');
+  backdrop.classList.add('is-hidden');
 
-    refs.openModalBtn.addEventListener("click", function () {
-        toggleModal();
-    });
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', function () {
+    toggleModal();
+  });
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-    function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-    }
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
 })();
 
 window.addEventListener('load', () => {
-    if (window.location.pathname.endsWith('favorites.html')) {
-        favoritesButton.classList.add('active-link');
-        homeButton.classList.remove('active-link');
-    } else {
-        homeButton.classList.add('active-link');
-        favoritesButton.classList.remove('active-link');
-    }
+  if (window.location.pathname.endsWith('favorites.html')) {
+    favoritesButton.classList.add('active-link');
+    homeButton.classList.remove('active-link');
+  } else {
+    homeButton.classList.add('active-link');
+    favoritesButton.classList.remove('active-link');
+  }
 });
--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,33 +1,36 @@
-import { getAccess } from "./helper/get-access";
+import { getAccess } from './helper/get-access';
 
 const backdrop = document.querySelector('.backdrop-thumb');
 const closeIcon = document.querySelector('.js-modal-window');
 const galleryWindow = document.querySelector('.js-gallery');
+const link = document.querySelector('.link-home');
+console.log(link);
+const isActiveLink = link.classList.contains('active-link');
 const currentPage = window.location.pathname;
 
 // Якщо поточна сторінка - index.html, то показуємо блок з кнопкою
-if (currentPage.includes('index.html')) {
-    const removeCards = document.querySelector(".buttons-modal");
-    if (removeCards) {
-        removeCards.classList.remove('visually-hidden');
-    }
+if (currentPage.includes('index.html') || isActiveLink) {
+  const removeCards = document.querySelector('.buttons-modal');
+  if (removeCards) {
+    removeCards.classList.remove('visually-hidden');
+  }
 }
 
 galleryWindow.addEventListener('click', openModal);
 
 // Відкриття модалки
 function openModal(e) {
-    if (e.target.classList.contains('js-start')) {
-        backdrop.classList.remove('is-open');
-        document.body.style.overflow = 'hidden';
+  if (e.target.classList.contains('js-start')) {
+    backdrop.classList.remove('is-open');
+    document.body.style.overflow = 'hidden';
 
-        const liElement = e.target.closest('.js-id');
+    const liElement = e.target.closest('.js-id');
 
-        if (liElement) {
-            const id = liElement.dataset.id;
-            getExercisesObject(id);
-        }
+    if (liElement) {
+      const id = liElement.dataset.id;
+      getExercisesObject(id);
     }
+  }
 }
 
 // Закриття модального вікна
@@ -35,79 +38,46 @@ closeIcon.addEventListener('click', closeModal);
 document.addEventListener('keydown', closeModalByEsc);
 
 function closeModal(e) {
-    backdrop.classList.add('is-open');
-    document.body.style.overflow = '';
+  backdrop.classList.add('is-open');
+  document.body.style.overflow = '';
 
-    clearModalContent();
+  clearModalContent();
 }
 
 function closeModalByEsc(e) {
-    if (e.code === 'Escape') {
-        backdrop.classList.add('is-open');
-        document.body.style.overflow = '';
-    }
+  if (e.code === 'Escape') {
+    backdrop.classList.add('is-open');
+    document.body.style.overflow = '';
+  }
 }
 backdrop.addEventListener('click', function (event) {
-    if (event.target === this) {
-        closeModal();
-    }
+  if (event.target === this) {
+    closeModal();
+  }
 });
-
 
 // Завантаження сторінки
 function getExercisesObject(id) {
-    getAccess({ typeFilter: 'exercises', id: id })
-        .then(({ data }) => {
-            const { name, rating, target, bodyPart, equipment, popularity, burnedCalories, description, gifUrl, _id } = data
+  getAccess({ typeFilter: 'exercises', id: id })
+    .then(({ data }) => {
+      const {
+        name,
+        rating,
+        target,
+        bodyPart,
+        equipment,
+        popularity,
+        burnedCalories,
+        description,
+        gifUrl,
+        _id,
+      } = data;
 
-            const modalWindow = document.querySelector('.modal-window');
-            modalWindow.setAttribute('data-modal-id', _id);
+      const modalWindow = document.querySelector('.modal-window');
+      modalWindow.setAttribute('data-modal-id', _id);
 
-
-            const refs = {
-                img: document.querySelector(".js-img"),
-                title: document.querySelector('.js-title'),
-                raiting: document.querySelector('.js-raiting'),
-                targetValue: document.querySelector('.js-target'),
-                bodyPartValue: document.querySelector('.js-body-part'),
-                equipmentValue: document.querySelector('.js-equipment'),
-                popularValue: document.querySelector('.js-popular'),
-                caloriesValue: document.querySelector('.js-calories'),
-                descriptionValue: document.querySelector('.js-description'),
-            }
-
-            const fixedRating = Math.round(rating).toFixed(1)
-
-            const stars = document.querySelectorAll('.raiting-item .icon-star');
-
-            stars.forEach((star, index) => {
-                if (index < Math.floor(fixedRating)) {
-                    star.classList.remove('non-activ');
-                } else if (index === Math.floor(fixedRating) && fixedRating % 1 !== 0) {
-                    star.classList.remove('non-activ');
-                } else {
-                    star.classList.add('non-activ');
-                }
-            });
-
-            refs.img.setAttribute('src', gifUrl);
-            refs.title.textContent = name.charAt(0).toUpperCase() + name.slice(1);
-            refs.raiting.textContent = fixedRating;
-            refs.targetValue.textContent = target;
-            refs.bodyPartValue.textContent = bodyPart;
-            refs.equipmentValue.textContent = equipment;
-            refs.popularValue.textContent = popularity;
-            refs.caloriesValue.textContent = burnedCalories;
-            refs.descriptionValue.textContent = description;
-
-        })
-        .catch((err) => console.error(err));
-}
-
-// Очіщюємо вміст модалки після закриття
-function clearModalContent() {
-    const refs = {
-        img: document.querySelector(".js-img"),
+      const refs = {
+        img: document.querySelector('.js-img'),
         title: document.querySelector('.js-title'),
         raiting: document.querySelector('.js-raiting'),
         targetValue: document.querySelector('.js-target'),
@@ -116,22 +86,63 @@ function clearModalContent() {
         popularValue: document.querySelector('.js-popular'),
         caloriesValue: document.querySelector('.js-calories'),
         descriptionValue: document.querySelector('.js-description'),
-    };
+      };
 
-    // Встановлюємо пусті значення для всіх елементів модального вікна
-    refs.img.setAttribute('src', '');
-    refs.title.textContent = '';
-    refs.raiting.textContent = '';
-    refs.targetValue.textContent = '';
-    refs.bodyPartValue.textContent = '';
-    refs.equipmentValue.textContent = '';
-    refs.popularValue.textContent = '';
-    refs.caloriesValue.textContent = '';
-    refs.descriptionValue.textContent = '';
+      const fixedRating = Math.round(rating).toFixed(1);
 
-    // Очищаємо класи зірок рейтингу
-    const stars = document.querySelectorAll('.raiting-item .icon-star');
-    stars.forEach(star => {
-        star.classList.add('non-activ');
-    });
+      const stars = document.querySelectorAll('.raiting-item .icon-star');
+
+      stars.forEach((star, index) => {
+        if (index < Math.floor(fixedRating)) {
+          star.classList.remove('non-activ');
+        } else if (index === Math.floor(fixedRating) && fixedRating % 1 !== 0) {
+          star.classList.remove('non-activ');
+        } else {
+          star.classList.add('non-activ');
+        }
+      });
+
+      refs.img.setAttribute('src', gifUrl);
+      refs.title.textContent = name.charAt(0).toUpperCase() + name.slice(1);
+      refs.raiting.textContent = fixedRating;
+      refs.targetValue.textContent = target;
+      refs.bodyPartValue.textContent = bodyPart;
+      refs.equipmentValue.textContent = equipment;
+      refs.popularValue.textContent = popularity;
+      refs.caloriesValue.textContent = burnedCalories;
+      refs.descriptionValue.textContent = description;
+    })
+    .catch(err => console.error(err));
+}
+
+// Очіщюємо вміст модалки після закриття
+function clearModalContent() {
+  const refs = {
+    img: document.querySelector('.js-img'),
+    title: document.querySelector('.js-title'),
+    raiting: document.querySelector('.js-raiting'),
+    targetValue: document.querySelector('.js-target'),
+    bodyPartValue: document.querySelector('.js-body-part'),
+    equipmentValue: document.querySelector('.js-equipment'),
+    popularValue: document.querySelector('.js-popular'),
+    caloriesValue: document.querySelector('.js-calories'),
+    descriptionValue: document.querySelector('.js-description'),
+  };
+
+  // Встановлюємо пусті значення для всіх елементів модального вікна
+  refs.img.setAttribute('src', '');
+  refs.title.textContent = '';
+  refs.raiting.textContent = '';
+  refs.targetValue.textContent = '';
+  refs.bodyPartValue.textContent = '';
+  refs.equipmentValue.textContent = '';
+  refs.popularValue.textContent = '';
+  refs.caloriesValue.textContent = '';
+  refs.descriptionValue.textContent = '';
+
+  // Очищаємо класи зірок рейтингу
+  const stars = document.querySelectorAll('.raiting-item .icon-star');
+  stars.forEach(star => {
+    star.classList.add('non-activ');
+  });
 }
