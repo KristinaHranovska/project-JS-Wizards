e3c90707c3bd066585f55b2779d7ce5aa21089c3
Sashaaapp <sashaaapp1980@gmail.com>
2024-02-13 21:51:27 +0200

    Update subscribe.js

--- src/js/subscribe.js
+++ src/js/subscribe.js
@@ -49,31 +49,3 @@ footerForm.addEventListener('input', (event) => {
     const formData = JSON.stringify({ footerEmail });
     localStorage.setItem(feedbackForm, formData);
 });
-
-// const subscribeValidity = async (event) => {
-//     event.preventDefault();
-
-//     const email = footerInput.value.trim();
-
-//     if (!emailPattern.test(email)) {
-//         iziToastFunctions.getErrorInfo('Enter the correct email!');
-//     } else {
-//         try {
-//             await postAccess({ userEmail: email, typeFilter: 'subscription' });
-//             iziToastFunctions.getSuccessInfo('Successful subscription!');
-//         } catch (error) {
-//             iziToastFunctions.getErrorInfo('The user with this address is already subscribed!');
-//         }
-
-//         localStorage.removeItem(feedbackForm);
-//         footerForm.reset();
-//     }
-// };
-
-// footerForm.addEventListener('submit', subscribeValidity);
-
-// footerForm.addEventListener('input', (event) => {
-//     const footerEmail = footerInput.value.trim();
-//     const formData = JSON.stringify({ footerEmail });
-//     localStorage.setItem(feedbackForm, formData);
-// });
